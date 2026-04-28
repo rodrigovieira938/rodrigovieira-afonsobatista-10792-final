704bcb9f36c31a5f513319f8d29522c622debce1
Rodrigo Vieira <rodizzvieira@gmail.com>
2026-04-28 11:37:22 +0100

    feat: add cors

--- Stockaholic.Api/Program.cs
+++ Stockaholic.Api/Program.cs
@@ -3,9 +3,16 @@ using Stockaholic.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<StockaholicDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -30,6 +37,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
 app.MapControllers();
 
