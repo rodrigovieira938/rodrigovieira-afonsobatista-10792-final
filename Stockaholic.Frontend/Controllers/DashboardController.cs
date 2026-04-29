27440da48deb26a220b957703bdafbfdb335da6b
Rodrigo Vieira <rodizzvieira@gmail.com>
2026-04-29 10:41:04 +0100

    feat: add api http client to Frontend

--- Stockaholic.Frontend/Controllers/DashboardController.cs
+++ Stockaholic.Frontend/Controllers/DashboardController.cs
@@ -1,11 +1,37 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Stockaholic.Frontend.Controllers;
 
 public class DashboardController : Controller
 {
-    public IActionResult Index()
+    private readonly IHttpClientFactory _clientFactory;
+    private readonly ILogger<DashboardController> _logger;
+
+    public DashboardController(IHttpClientFactory clientFactory, ILogger<DashboardController> logger)
     {
-        return View();
+        _clientFactory = clientFactory;
+        _logger = logger;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var client = _clientFactory.CreateClient("ApiClient");
+
+        try
+        {
+            //Just dummy call to test resilience policies. Replace with actual API call and model binding as needed.
+            var response = await client.GetAsync("produtos");
+            response.EnsureSuccessStatusCode();
+
+            var products = await response.Content.ReadAsStringAsync();
+            _logger.LogInformation("Produtos: {}", products);
+            return View();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred while fetching products");
+            return StatusCode(500, "Internal Server Error");
+        }
     }
 }
--- Stockaholic.Frontend/Program.cs
+++ Stockaholic.Frontend/Program.cs
@@ -3,8 +3,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Polly;
-using Polly.Retry;
-using Polly.Timeout;
+using Polly.Extensions.Http;
+using Microsoft.Extensions.Http.Resilience;
+using System.Net.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,22 @@ builder.Services.AddRouting(options =>
     options.LowercaseUrls = true;
     options.LowercaseQueryStrings = true;
 });
+
+builder.Services.AddHttpClient("ApiClient", client =>
+    client.BaseAddress = new Uri(builder.Configuration["ApiSettings:BaseUrl"]!))
+    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = 
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    })
+    .AddStandardResilienceHandler(options =>
+    {
+        options.Retry.MaxRetryAttempts = 3;
+        options.Retry.Delay = TimeSpan.FromSeconds(2);
+        options.CircuitBreaker.SamplingDuration = TimeSpan.FromSeconds(60);
+        options.TotalRequestTimeout.Timeout = TimeSpan.FromSeconds(15);
+    })
+    ;
 
 var app = builder.Build();
 
